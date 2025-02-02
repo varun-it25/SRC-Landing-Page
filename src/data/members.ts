fc0645f9f8e2aac9d620ff7859894fe3ef930722
varun kangotra <varunkangotra.it25@gmail.com>
2025-02-02 21:39:24 +0530

    Member Key Update

--- src/data/members.ts
+++ src/data/members.ts
@@ -24,7 +24,7 @@ export const memberData = [
         email: `aryanarya.it25@gmail.com`,
     },
     {
-        memberId: 3,
+        memberId: 4,
         name: `Yodan`,
         desination: `Video Editor`,
         image: `https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg`,
