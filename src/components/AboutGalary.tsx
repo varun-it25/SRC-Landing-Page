1c61bdc7f4d4d5e4c19e75dd5737052eb0f2e045
Avi Maheshwari <avimaheshwari04@gmail.com>
2025-02-16 14:41:57 +0530

    add about Gallery

--- src/components/AboutGalary.tsx
+++ src/components/AboutGalary.tsx
@@ -1,55 +1,72 @@
+import library from "@/assets/Library.jpg";
+import wisdom from "@/assets/wisdom hall.png";
+import meditation from "@/assets/meditation_hall.jpg";
+
 const AboutGalary = () => {
   return (
-    <section className='flex-col bg-blue-50 h-fit sm:h-[90vh] justify-between items-center sm:flex-row flex space-x-6 px-5 md:px-20 sm:px-12 py-7 sm:py-12'>
+    <section className="flex-col bg-blue-50 h-fit sm:h-[90vh] justify-between items-center sm:flex-row flex space-x-6 px-5 md:px-20 sm:px-12 py-7 sm:py-12 mb-10">
       <div className="grid md:grid-cols-3">
-        {/* Learning Centre */}
-        <div className="bg-orange-400 p-6 text-center">
-          <h2 className="text-2xl font-semibold">Learning Centre</h2>
-          <br/>
+        {/* Library */}
+        <div className="bg-orange-400 px-2 text-center flex flex-col justify-center items-center h-full">
+          <h2 className="text-3xl font-semibold">Spiritual Library</h2>
+          <br />
           <p>
-            ‘Training Centre’ comprises of 2 board rooms, 5 training halls of 150 seating
-            capacity & a mini auditorium of 450 seating capacity.
+            Bringing closer the self, the supreme, and the society, our
+            Spiritual Library is a sanctuary of wisdom and inspiration, filled
+            with self-help books which offer a peaceful space for reflection and
+            learning.
           </p>
         </div>
 
         {/* Image */}
         <div>
-          <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Learning Centre" className="w-full h-56 object-cover" />
+          <img
+            src={meditation}
+            alt="Learning Centre"
+            className="w-full h-70 object-cover"
+          />
         </div>
 
-         {/* Meditation */}
-        <div className="bg-teal-400 p-6 text-center">
-          <h2 className="text-2xl font-semibold">Meditation</h2>
-          <br/>
+        {/* Meditation */}
+        <div className="bg-teal-400 px-2 text-center flex flex-col justify-center items-center h-full">
+          <h2 className="text-3xl font-semibold">Wisdom Hall</h2>
+          <br />
           <p>
-            A large meditation room and a pyramid are located at the campus for experiencing
-            deep meditation experience and inner silence.
+            Combining virtue and wit, to ignite the brave leader in a student
+            where teaching become pearls of the ocean of knowledge.
           </p>
         </div>
 
         {/* Meditation Image */}
         <div>
-          <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Meditation" className="w-full h-56 object-cover" />
+          <img
+            src={library}
+            alt="Meditation"
+            className="w-full h-70 object-cover"
+          />
         </div>
 
         {/* Other Facilities */}
-        <div className="bg-cyan-400 p-6 text-center">
-          <h2 className="text-2xl font-semibold">Other Facilities</h2>
-          <br/>
+        <div className="bg-cyan-400 px-2 text-center flex flex-col justify-center items-center h-full">
+          <h2 className="text-3xl font-semibold">Meditation Room</h2>
+          <br />
           <p>
-            ‘Training Centre’ comprises of 2 board rooms, 5 training halls of 150 seating.
+            A place of great serenity and self-reflection where students can
+            practice meditation in a peaceful and harmonious environment.
           </p>
         </div>
 
         {/* Facilities Image */}
         <div>
-          <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Other Facilities" className="w-full h-56 object-cover" />
+          <img
+            src={wisdom}
+            alt="Other Facilities"
+            className="w-full h-70 object-cover"
+          />
         </div>
       </div>
     </section>
-    
-  )
-}
+  );
+};
 
-export default AboutGalary
-
+export default AboutGalary;
