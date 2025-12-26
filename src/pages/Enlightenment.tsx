c91d1f82ef92ac46eeba17520d67459c129f586a
Avi Maheshwari <avimaheshwari04@gmail.com>
2025-12-26 13:18:33 +0530

    Fix Error

--- src/pages/Enlightenment.tsx
+++ src/pages/Enlightenment.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
